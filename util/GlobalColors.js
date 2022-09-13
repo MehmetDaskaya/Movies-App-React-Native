505e488bb6b7b4e638ffdd3b702deb3e0364a035
Mehmet D <55313337+Nerimb@users.noreply.github.com>
2022-09-14 01:07:03 +0300

    Add files via upload

---
+++ util/GlobalColors.js
@@ -0,0 +1,11 @@
+ const GlobalColors = {
+    Navy: "#2B2E4A",
+    DarkBlue: "#223343",
+    Red: "#E84545",
+    Maroon: "#903749",
+    Black: "#53354A",
+    Grey: "#868686",
+    Yellow: "#F0D43A",
+}
+
+export default GlobalColors;
